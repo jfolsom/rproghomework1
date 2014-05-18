ba546ec0fa9bf8c4475864450034a485b1642b9c
Jacob Folsom <cercops@gmail.com>
2014-05-18 15:35:39 -0400

    Now returns mean sulfate data across requested ids

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a repository for Coursera Course rprog-003 assignment 1.
--- pollutantmean.R
+++ pollutantmean.R
@@ -1,15 +1,12 @@
-pollutantmean <- function(directory, date) {
-  # Returns the mean sulfate reading for a given date from the files in the
-  # directory.  Currently set to only read the first 5 files.
-  FileList <- list.files(directory, full.names = TRUE)
-  # NumberFiles <- length(FileList)
+pollutantmean <- function(directory, id) {
+  # Returns the mean sulfate reading for a
+  FileList <- list.files(directory, full.names = TRUE)  
   PolData <- data.frame()
-  for(i in 1:5) {
-    # Puts the data from all files in the directory into one data frame
+  for(i in id) {
+    # Puts the data from all files from the id list in one data frame
     PolData <- rbind(PolData, read.csv(FileList[i]))
   } 
-  DateData <- PolData[PolData[, "Date"] == date, ]  # The data for that date
-  MeanSulfateDate <- mean(DateData$sulfate, na.rm = TRUE)
+  MeanSulfate <- mean(PolData$sulfate, na.rm = TRUE)
   # Calculate mean while stripping NAs
-  MeanSulfateDate
+  MeanSulfate
 }
