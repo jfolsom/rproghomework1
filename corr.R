08b92991d0370c00bb0f16da8168749fce7a0a77
Jacob Folsom <cercops@gmail.com>
2014-05-20 08:13:12 -0400

    part 3 drafted

---
+++ corr.R
@@ -0,0 +1,25 @@
+corr <- function(directory, threshold = 0) {
+  ## 'directory' is a character vector of length 1 indicating
+  ## the location of the CSV files
+  
+  ## 'threshold' is a numeric vector of length 1 indicating the
+  ## number of completely observed observations (on all
+  ## variables) required to compute the correlation between
+  ## nitrate and sulfate; the default is 0
+  
+  ## Return a numeric vector of correlations
+  
+  Files.Long <- list.files(directory, full.names = TRUE)
+  Corr.Vector <- vector(mode = "numeric")
+  
+  for(i in 1:length(Files.Long)) {
+    Monitor.Data <- read.csv(Files.Long[i])
+    Number.Complete <- sum(complete.cases(Monitor.Data))
+    if(Number.Complete >= threshold) {
+      Correlation <- cor(Monitor.Data$sulfate, Monitor.Data$nitrate,
+                         use = "na.or.complete")
+      Corr.Vector <- c(Corr.Vector, round(Correlation, 5))
+    }
+  } 
+  Corr.Vector
+}
